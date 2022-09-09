0591b8ae321600d15f144dd70b99ced053e8581a
Kingshaq <iyiolainioluwa@gmail.com>
2022-09-08 21:40:29 -0700

    rise

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -14,14 +14,13 @@
 
    *this program prints "Programming is like building a multilingual puzzle"                                                           
 
-   *Return: 0                                                                                                                          
+   *Return: (0)                                                                                                                          
 
    */
 
 int main (void)
 
 {
-  
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
