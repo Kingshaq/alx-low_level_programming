662694c95f936d2f19337a2af9b761aa7ab559cb
Kingshaq <iyiolainioluwa@gmail.com>
2022-09-08 21:47:54 -0700

    Little did i know

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -21,6 +21,7 @@
 int main (void)
 
 {
-  puts("\"Programming is like building a multilingual puzzle");
+	/*using the	puts	function*/
+	puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
